ed7601bba1df6ac7ac65bb658232a52ec24b3fb7
Satomin2864 <satomiyuta0602@gmail.com>
2017-01-27 19:16:52 +0900

    重さの変更

--- nanor.py
+++ nanor.py
@@ -11,7 +11,7 @@ def NAND(x1, x2):
 def OR(x1, x2):
     x = np.array([x1,x2])
     w = np.array([0.5, 0.5])# 重みとバイアスが違う
-    b = 0.7
+    b = 0.2
     tmp = np.sum(w*x) + b
     if tmp <= 0:
         return 0
